bdcd4963a415281c9b9eed34acbac262281f216a
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2022-07-31 17:50:32 +0700

    CRUD product

--- modules/product/product.controller.js
+++ modules/product/product.controller.js
@@ -1,8 +1,29 @@
 const ProductModel = require("./product");
 
 // GET ALL
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await
+      ProductModel.find()
+        // .populate({ path: 'productId', select: 'title' })
+        // .populate('createdBy', 'username');
 
-// GET
+    res.send({
+      success: 1,
+      data: products
+    })
+  } catch (err) {
+    res
+      .status(400)
+      .send({
+        success: 0,
+        data: null,
+        message: err.message || 'Something went wrong'
+      })
+  }
+}
+
+// GET By ID
 const getProduct = async (req, res) => {
   const { productId } = req.params;
 
@@ -28,10 +49,45 @@ const createProduct = async (req, res) => {
 };
 
 // UPDATE
+const updateProduct = async (req, res) => {
+  const { productId } = req.params;
 
-//DELETE
+  const updateProductData = req.body;
+
+  const updatedProduct = await ProductModel.findOneAndUpdate(
+    { _id: productId },
+    updateProductData,
+    { new: true }
+  );
+
+  if (!updatedProduct) {
+    throw new Error("Not found product");
+  }
+
+  res.send({
+    success: 1,
+    data: updatedProduct,
+  });
+};
+
+// DELETE
+const deleteProduct = async (req, res) => {
+  const { productId } = req.params;
+
+  const deletedProduct = await ProductModel.findOneAndDelete(
+    { _id: productId }
+  );
+
+  res.send({
+    success: 1,
+    data: deletedProduct,
+  });
+};
 
 module.exports = {
+  getAllProducts,
   getProduct,
   createProduct,
+  updateProduct,
+  deleteProduct
 };
--- modules/product/product.router.js
+++ modules/product/product.router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router(); // khởi tạo object
 const productController = require("./product.controller");
 
-
+router.get('/', productController.getAllProducts);
 router.get("/:productId", productController.getProduct);
 router.post(
     '/',
@@ -9,5 +9,7 @@ router.post(
     // isAuth,
     productController.createProduct
 );
+router.put("/:productId", productController.updateProduct);
+router.delete('/:productId', productController.deleteProduct);
 
 module.exports = router;

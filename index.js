fe7bb2c5e8f592cd96be467e651b323effd8b745
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2023-01-16 01:14:58 +0700

    connect DB & run server

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express"); // lấy module bên thứ 3
+const mongoose = require("mongoose");
+const cors = require("cors");
+// const productRoute = require('./modules/product');
+// const categoryRoute = require('./modules/category');
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URL);
+  console.log("DB connection successfuly!");
+
+  const app = express();
+  app.use(cors())
+  app.use(express.json());
+
+//   app.use("/api/products", productRoute);
+//   app.use("/api/categorys", categoryRoute);
+
+  app.listen(process.env.PORT || 8800, (err) => {
+    if (err) throw err;
+    console.log("Backend server is running!");
+  });
+}
+
+main();
